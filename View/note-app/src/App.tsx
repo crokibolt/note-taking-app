0ae10d407227b84793d658f2d7edf8b2167d71b3
crokibolt <manuelmmendoza4@gmail.com>
2023-08-14 18:37:43 -0300

    removed unused page, first version of the front end finished

--- View/note-app/src/App.tsx
+++ View/note-app/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import New from "./pages/New";
-import Edit from "./pages/Edit";
 import Note from "./pages/Note";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/note/:id" element={<Note />} />
-        <Route path="/edit/:id" element={<Edit />} />
       </Routes>
     </>
   );
