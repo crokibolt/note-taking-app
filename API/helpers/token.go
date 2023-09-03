9eddcef784dc2fc8a48f74a84b40ea0c3e090ec4
crokibolt <manuelmmendoza4@gmail.com>
2023-09-03 14:17:58 -0300

    changed cookie names and lifespans

--- API/helpers/token.go
+++ API/helpers/token.go
@@ -31,9 +31,9 @@ func GenerateToken(user_id uint, ctx *gin.Context) (string, error) {
 		return "", err
 	}
 
-	ctx.SetCookie("token", tokenString, token_lifespan*3600, "/", "localhost:5173", false, true)
+	ctx.SetCookie("sesion-cookie", tokenString, 3600, "/", "localhost:5173", false, true)
 	// ctx.SetCookie("sample-cookie", "sample content", token_lifespan*3600, "/", "note-api-v1.onrender.com", false, false)
-	ctx.SetCookie("sample-cookie", "sample content", token_lifespan*3600, "/", "localhost:5173", false, false)
+	ctx.SetCookie("sample-cookie", "sample content", 3600, "/", "localhost:5173", false, false)
 
 	return "Successful login", nil
 }
